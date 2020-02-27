65f8353ca0e0e96ea4df9270337f456a4f3bbbc8
Scott <ryukaider@gmail.com>
2020-02-26 22:13:26 -0800

    Fix formatting

--- stocks/database/postgres.py
+++ stocks/database/postgres.py
@@ -1,19 +1,21 @@
 import psycopg2
 
+
 def connect(username, password, host, port, database):
     try:
-        connection = psycopg2.connect(user = username,
-                                      password = password,
-                                      host = host,
-                                      port = port,
-                                      database = database)
+        connection = psycopg2.connect(user=username,
+                                      password=password,
+                                      host=host,
+                                      port=port,
+                                      database=database)
         connection.autocommit = True
         return connection
-    except (Exception, psycopg2.Error) as error :
-        print ("Error while connecting to PostgreSQL", error)
+    except (Exception, psycopg2.Error) as error:
+        print("Error while connecting to Postgres SQL", error)
     return None
 
-def close_connection(connection, cursor = None):
+
+def close_connection(connection, cursor=None):
     if cursor is not None:
         cursor.close()
     if connection is not None:
@@ -22,25 +24,31 @@ def close_connection(connection, cursor = None):
         return True
     return False
 
+
 def get_version(cursor):
     query = "SELECT version();"
     run_query(cursor, query)
     record = cursor.fetchone()
-    print("You are connected to - ", record,"\n")
+    print("You are connected to - ", record, "\n")
+
 
 def create_database():
     raise NotImplementedError
 
+
 def create_table():
     raise NotImplementedError
+
 
 def add_column(cursor, table, column, typeName):
     query = f'ALTER TABLE {table} ADD COLUMN "{column}" {typeName};'
     return run_query(cursor, query)
 
+
 def delete_column(cursor, table, column):
     query = f'ALTER TABLE {table} DROP COLUMN "{column}";'
     return run_query(cursor, query)
+
 
 def insert_row_dict(cursor, table, dictionary):
     columns = '('
@@ -54,10 +62,12 @@ def insert_row_dict(cursor, table, dictionary):
     success = run_query(cursor, query)
     return success
 
+
 def insert_row(cursor, table, columns, values):
     query = f'INSERT INTO {table} {columns} VALUES {values};'
     success = run_query(cursor, query)
     return success
+
 
 def update_row(cursor, table, searchColumn, searchValue, setColumn, setValue):
     query = f'UPDATE {table} SET {setColumn} = {setValue} WHERE {searchColumn} = {searchValue}'
@@ -65,6 +75,7 @@ def update_row(cursor, table, searchColumn, searchValue, setColumn, setValue):
     if success:
         print(f'Successfully updated row into table {table}')
     return success
+
 
 """
 def upsert(cursor, table, columnList, valueList):
@@ -76,12 +87,14 @@ def upsert(cursor, table, columnList, valueList):
     return success
 """
 
+
 def remove_row(cursor, table, column, value):
     query = f"DELETE FROM {table} WHERE {column} = '{value}';"
     success = run_query(cursor, query)
     if success:
         print(f'Successfully removed {cursor.rowcount} row(s) from table {table}')
-    
+
+
 def run_query(cursor, query):
     print(query)
     try:
@@ -90,6 +103,7 @@ def run_query(cursor, query):
     except Exception as error:
         print(f'Postgres error: {error}')
         return False
+
 
 def get_list_results(cursor):
     rows = cursor.fetchall()
