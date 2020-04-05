02679cc9ac7b566c4851ef674b69c334cf820819
Scott <ryukaider@gmail.com>
2020-04-04 21:46:29 -0700

    api_progress table can create itself

--- stocks/database/tables/api_progress_table.py
+++ stocks/database/tables/api_progress_table.py
@@ -1,11 +1,32 @@
-import sys
-sys.path.append('E:/Google Drive/Computers/Dev/Stocks/scott_stocks')
+import os, sys
+_root_path = os.path.join(os.path.dirname(__file__), '..', '..')
+sys.path.append(_root_path)
 
 from database import postgres
 from database import stocks_database
 from database.tables import tickers_table
 
+
 table_name = 'api_progress'
+
+columns = {
+    'ticker': 'text PRIMARY KEY',
+    'monthly': 'boolean',
+    'eps': 'boolean'
+}
+
+cursor = stocks_database.get_cursor()
+
+
+def exists():
+    return postgres.table_exists(cursor, table_name)
+
+
+def create():
+    if not exists():
+        return postgres.create_table(cursor, table_name, columns)
+    return exists()
+
 
 def reset_all():
     ticker_list = tickers_table.get_tickers()
@@ -14,34 +35,42 @@ def reset_all():
             reset_monthly_progress(ticker)
             reset_eps_progress(ticker)
 
+
 def add_stock(ticker):
     return postgres.insert_row(
-        stocks_database.cursor,
+        cursor,
         table_name,
         '(ticker, monthly, eps)',
         f"('{ticker}',false,false)")
 
+
 def reset_monthly_progress(ticker):
     _update_progress(ticker, 'monthly', False)
+
 
 def set_monthly_done(ticker):
     _update_progress(ticker, 'monthly', True)
 
+
 def reset_eps_progress(ticker):
     _update_progress(ticker, 'eps', False)
+
 
 def set_eps_done(ticker):
     _update_progress(ticker, 'eps', True)
 
+
 def _update_progress(ticker, column, value):
     postgres.update_row(
-        stocks_database.cursor, table_name, 'ticker', f"'{ticker}'", column, value)
+        cursor, table_name, 'ticker', f"'{ticker}'", column, value)
+
 
 def get_incomplete_stocks(column):
     query = f"SELECT ticker FROM {table_name} WHERE {column} is not true ORDER BY ticker ASC"
-    postgres.run_query(stocks_database.cursor, query)
-    tickers = postgres.get_list_results(stocks_database.cursor)
+    postgres.run_query(cursor, query)
+    tickers = postgres.get_list_results(cursor)
     return tickers
 
-if __name__ == "__main__":
-    reset_all()
+
+create()
+reset_all()
