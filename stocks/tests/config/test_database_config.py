c229854d31c401006f813225753b11bd0885881e
Scott <ryukaider@gmail.com>
2020-04-04 22:32:00 -0700

    Add tests for database_config

---
+++ stocks/tests/config/test_database_config.py
@@ -0,0 +1,34 @@
+import os, sys
+_root_path = os.path.join(os.path.dirname(__file__), '..', '..')
+sys.path.append(_root_path)
+
+import pytest
+from config import database_config
+
+
+def test_config_path():
+    assert database_config._config_path is not None
+
+
+def test_database_config_path():
+    assert database_config._database_config_path is not None
+
+
+def test_username():
+    assert database_config.username is not None
+
+
+def test_host():
+    assert database_config.host is not None
+
+
+def test_port():
+    assert database_config.port is not None
+
+
+def test_database():
+    assert database_config.database is not None
+
+
+def test_test_database():
+    assert database_config.test_database is not None
