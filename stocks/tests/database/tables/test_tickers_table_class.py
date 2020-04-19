aa92098b900ca0db6bb7b812df6bf603467d02a1
Scott <ryukaider@gmail.com>
2020-04-18 21:17:22 -0700

    Add test cases for tickers table class

---
+++ stocks/tests/database/tables/test_tickers_table_class.py
@@ -0,0 +1,45 @@
+import os, sys
+_root_path = os.path.join(os.path.dirname(__file__), '..', '..', '..')
+sys.path.append(_root_path)
+
+import pytest
+from database.tables.tickers_table_class import TickersTable
+from stock import Stock
+from utilities import random_utilities
+
+table_name = 'testtickerstable'
+table = TickersTable(table_name)
+
+
+def test_exists():
+    assert table.exists() is True
+
+
+def test_create():
+    assert table.create() is True
+
+
+def test_add_remove_stock():
+    stock = create_random_stock()
+    assert table.add_stock(stock) is True
+    assert table.remove_stock(stock) is True
+
+
+def test_add_remove_stocks():
+    stocks = []
+    stocks.append(create_random_stock())
+    stocks.append(create_random_stock())
+    assert table.add_stocks(stocks) is True
+    assert table.remove_stocks(stocks) is True
+
+
+def test_get_tickers():
+    assert table.get_tickers() is not None
+
+
+def create_random_stock():
+    stock = Stock()
+    stock.ticker = random_utilities.random_letters(4)
+    stock.exchange = random_utilities.random_letters(6)
+    stock.name = random_utilities.random_letters(8)
+    return stock
