7d9f2128ce1ee83d47301818b910f3307eb78f6b
Scott <ryukaider@gmail.com>
2020-04-25 13:17:06 -0700

    Monthly history table (#4)
    * Update monthly history table to use table base class
    
    * Add tests for monthly history table

--- stocks/database/tables/monthly_history_table.py
+++ stocks/database/tables/monthly_history_table.py
@@ -1,75 +1,58 @@
-import os, sys
-_root_path = os.path.join(os.path.dirname(__file__), '..', '..')
-sys.path.append(_root_path)
-
 from database import postgres
-from database import stocks_database
+from database.tables.table import Table
+
+class MonthlyHistoryTable(Table):
+
+    columns = {
+        'ticker': 'text NOT NULL',
+        'date': 'date NOT NULL',
+        'price': 'double precision',
+        'dividend': 'double precision',
+        'UNIQUE': '(ticker, date)'
+    }
+
+    def __init__(self, table_name='monthly_history'):
+        Table.__init__(self, table_name)
 
 
-table_name = 'monthly_history'
-
-columns = {
-    'ticker': 'text NOT NULL',
-    'date': 'date NOT NULL',
-    'price': 'double precision',
-    'dividend': 'double precision',
-    'CONSTRAINT pk_ticker_date': 'PRIMARY KEY (ticker, date)'
-}
-
-cursor = stocks_database.get_cursor()
+    def add_monthly_data(self, monthly_data):
+        for row in monthly_data:
+            add_monthly_row(row)
 
 
-def exists():
-    return postgres.table_exists(cursor, table_name)
+    def add_monthly_row(self, monthly_row):
+        if not postgres.insert_row(self.cursor, self.table_name, '(ticker, date, price, dividend)',
+            f"('{monthly_row['ticker']}','{monthly_row['date']}','{monthly_row['price']}','{monthly_row['dividend']}')"):
+            update_query = f"UPDATE {self.table_name} SET price = '{monthly_row['price']}' WHERE ticker = '{monthly_row['ticker']}' AND date = '{monthly_row['date']}'"
+            postgres.run_query(self.cursor, update_query)
 
 
-def create():
-    if not exists():
-        return postgres.create_table(cursor, table_name, columns)
-    return exists()
+    def get_history(self, ticker):
+        query = f"SELECT * FROM {self.table_name} WHERE ticker = '{ticker}' ORDER BY date DESC"
+        postgres.run_query(self.cursor, query)
+        data = self.cursor.fetchall()
+        return data
 
 
-def add_monthly_data(monthly_data):
-    for row in monthly_data:
-        add_monthly_row(row)
+    def get_date_dividend(self, ticker):
+        query = f"SELECT date,dividend FROM {self.table_name} WHERE ticker = '{ticker}' ORDER BY date DESC"
+        postgres.run_query(self.cursor, query)
+        data = self.cursor.fetchall()
+        return data
 
 
-def add_monthly_row(monthly_row):
-    if not postgres.insert_row(cursor, table_name, '(ticker, date, price, dividend)',
-        f"('{monthly_row['ticker']}','{monthly_row['date']}','{monthly_row['price']}','{monthly_row['dividend']}')"):
-        update_query = f"UPDATE {table_name} SET price = '{monthly_row['price']}' WHERE ticker = '{monthly_row['ticker']}' AND date = '{monthly_row['date']}'"
-        postgres.run_query(cursor, update_query)
+    def get_dividend_ttm(self, ticker):
+        query = f"SELECT dividend FROM {self.table_name} WHERE ticker = '{ticker}' ORDER BY date DESC LIMIT 12"
+        postgres.run_query(self.cursor, query)
+        dividends = postgres.get_list_results(self.cursor)
+        return dividends
 
 
-def get_history(ticker):
-    query = f"SELECT * FROM {table_name} WHERE ticker = '{ticker}' ORDER BY date DESC"
-    postgres.run_query(cursor, query)
-    data = cursor.fetchall()
-    return data
-
-
-def get_date_dividend(ticker):
-    query = f"SELECT date,dividend FROM {table_name} WHERE ticker = '{ticker}' ORDER BY date DESC"
-    postgres.run_query(cursor, query)
-    data = cursor.fetchall()
-    return data
-
-
-def get_dividend_ttm(ticker):
-    query = f"SELECT dividend FROM {table_name} WHERE ticker = '{ticker}' ORDER BY date DESC LIMIT 12"
-    postgres.run_query(cursor, query)
-    dividends = postgres.get_list_results(cursor)
-    return dividends
-
-
-def get_latest_price(ticker):
-    query = f"SELECT price FROM {table_name} WHERE ticker = '{ticker}' ORDER BY date DESC LIMIT 1"
-    postgres.run_query(cursor, query)
-    try:
-        price = postgres.get_list_results(cursor)[0]
-        return price
-    except:
-        return None
-
-
-create()
+    def get_latest_price(self, ticker):
+        query = f"SELECT price FROM {self.table_name} WHERE ticker = '{ticker}' ORDER BY date DESC LIMIT 1"
+        postgres.run_query(self.cursor, query)
+        try:
+            price = postgres.get_list_results(self.cursor)[0]
+            return price
+        except:
+            return None
---
+++ stocks/tests/database/tables/test_monthly_history_table.py
@@ -0,0 +1,61 @@
+import os, sys
+_root_path = os.path.join(os.path.dirname(__file__), '..', '..', '..')
+sys.path.append(_root_path)
+
+import pytest
+from utilities import random_utilities
+from database.tables.monthly_history_table import MonthlyHistoryTable
+
+table_name = 'test_monthly_history'
+test_ticker = 'TEST'
+monthly_history_table = MonthlyHistoryTable(table_name)
+
+monthly_history_table.add_monthly_row({
+        'ticker': test_ticker,
+        'date': '2020-04-25',
+        'price': random_utilities.random_double(),
+        'dividend': random_utilities.random_double()
+    })
+
+
+def test_exists():
+    assert monthly_history_table.exists()
+
+
+def test_create():
+    assert monthly_history_table.create()
+
+
+@pytest.mark.skip()
+def test_add_monthly_data():
+    monthly_history_table.add_monthly_data()
+
+
+def test_add_monthly_row():
+    monthly_row = {
+        'ticker': random_utilities.random_letters(8),
+        'date': '2020-04-25',
+        'price': random_utilities.random_double(),
+        'dividend': random_utilities.random_double()
+    }
+    monthly_history_table.add_monthly_row(monthly_row)
+
+
+def test_get_history():
+    history = monthly_history_table.get_history(test_ticker)
+    assert history is not None
+
+
+def test_get_date_dividend():
+    history = monthly_history_table.get_date_dividend(test_ticker)
+    assert history is not None
+
+
+def test_get_dividend_ttm():
+    data = monthly_history_table.get_dividend_ttm(test_ticker)
+    assert data is not None
+
+
+def test_get_latest_price():
+    price = monthly_history_table.get_latest_price(test_ticker)
+    assert price is not None
