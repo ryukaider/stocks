74262ce6632ca7fb10b60abcb6ff260b90a1e19e
Scott <ryukaider@gmail.com>
2020-05-10 00:58:56 +0000

    Iex (#12)
    * Add iex
    
    * Add IEX API

--- stocks/config/keys_config.py
+++ stocks/config/keys_config.py
@@ -7,3 +7,4 @@ _keys_config_path = os.path.join(_config_path, 'keys_config.json')
 
 database_password = json_utilities.read_json_file(_keys_config_path)['database_password']
 alpha_vantage_api_key = json_utilities.read_json_file(_keys_config_path)['alpha_vantage_api_key']
+iex_cloud_api_key = json_utilities.read_json_file(_keys_config_path)['iex_cloud_api_key']
---
+++ stocks/web_apis/iex.py
@@ -0,0 +1,14 @@
+import requests
+from config import keys_config
+
+base_url = 'https://cloud.iexapis.com/v1'
+token = keys_config.iex_cloud_api_key
+
+
+def get_company_profile(ticker):
+    url = f'{base_url}/stock/{ticker}/company?token={token}'
+    print(url)
+    try:
+        return requests.get(url).json()
+    except KeyError:
+        return None
