3ffc5a55a0bb649240609296c1f37329b6b701b4
Scott <ryukaider@gmail.com>
2020-05-27 22:33:12 -0700

    Get all tickers from nasdaq ftp (#29)

--- stocks/tests/web_apis/test_nasdaq.py
+++ stocks/tests/web_apis/test_nasdaq.py
@@ -1,8 +1,8 @@
 from web_apis import nasdaq
 
 
-def test_get_tickers():
-    tickers = nasdaq.get_tickers()
+def test_get_all_tickers():
+    tickers = nasdaq.get_all_tickers()
     assert len(tickers) > 0
     for ticker in tickers:
         assert isinstance(ticker, str) is True
--- stocks/web_apis/nasdaq.py
+++ stocks/web_apis/nasdaq.py
@@ -1,32 +1,49 @@
 from ftplib import FTP
 
 
-def get_tickers():
+def get_all_tickers():
+    tickers = []
+    tickers.extend(get_nasdaq_tickers())
+    tickers.extend(get_nyse_tickers())
+    return tickers
+
+
+def get_nasdaq_tickers():
     nasdaqlisted = _get_nasdaqlisted()
-    tickers = _extract_tickers_from_nasdaqlisted(nasdaqlisted)
+    tickers = _extract_tickers_from_text(nasdaqlisted)
+    return tickers
+
+
+def get_nyse_tickers():
+    otherlisted = _get_otherlisted()
+    tickers = _extract_tickers_from_text(otherlisted)
     return tickers
 
 
 def _get_nasdaqlisted():
+    return _get_text_from_ftp('nasdaqlisted.txt')
+
+
+def _get_otherlisted():
+    return _get_text_from_ftp('otherlisted.txt')
+
+
+def _get_text_from_ftp(filename):
     ftp = FTP('ftp.nasdaqtrader.com')
     ftp.login()
     binarydata = []
-    ftp.retrbinary("RETR /symboldirectory/nasdaqlisted.txt", callback=binarydata.append)
-    stringdata = ''
+    ftp.retrbinary(f'RETR /symboldirectory/{filename}', callback=binarydata.append)
+    text = ''
     for row in binarydata:
-        stringdata += row.decode("utf-8")
-    return stringdata
+        text += row.decode("utf-8")
+    return text
 
 
-def _extract_tickers_from_nasdaqlisted(nasdaqlisted):
-    splitdata = nasdaqlisted.splitlines()
+def _extract_tickers_from_text(text):
+    splitdata = text.splitlines()
     splitdata.pop(0)  # Remove header row
     tickers = []
     for line in splitdata:
         ticker = line.split('|')[0]
         tickers.append(ticker)
     return tickers
-
-
-if __name__ == '__main__':
-    get_tickers()
