7a893d2881a12c586741035546d5b192063e8506
Scott <ryukaider@gmail.com>
2020-06-07 21:43:09 -0700

    Fix imports for stocks database (#43)

--- stocks/databases/stocks_database.py
+++ stocks/databases/stocks_database.py
@@ -1,5 +1,10 @@
 from databases.database import Database
-from databases import tables
+from databases.tables.api_progress_table import ApiProgressTable
+from databases.tables.company_profile_table import CompanyProfileTable
+from databases.tables.daily_history_table import DailyHistoryTable
+from databases.tables.dividends_table import DividendsTable
+from databases.tables.tickers_table import TickersTable
+from databases.tables.yearly_history_table import YearlyHistoryTable
 
 
 class StocksDatabase(Database):
@@ -8,9 +13,9 @@ class StocksDatabase(Database):
 
         self.name = name
 
-        self.tickers_table = tables.tickers_table.TickersTable(self.cursor())
-        self.api_progress_table = tables.api_progress_table.ApiProgressTable(self.cursor())
-        self.company_profile_table = tables.company_profile_table.CompanyProfileTable(self.cursor())
-        self.daily_history_table = tables.daily_history_table.DailyHistoryTable(self.cursor())
-        self.yearly_history_table = tables.yearly_history_table.YearlyHistoryTable(self.cursor())
-        self.dividends_table = tables.dividends_table.DividendsTable(self.cursor())
+        self.tickers_table = TickersTable(self.cursor())
+        self.api_progress_table = ApiProgressTable(self.cursor())
+        self.company_profile_table = CompanyProfileTable(self.cursor())
+        self.daily_history_table = DailyHistoryTable(self.cursor())
+        self.yearly_history_table = YearlyHistoryTable(self.cursor())
+        self.dividends_table = DividendsTable(self.cursor())
