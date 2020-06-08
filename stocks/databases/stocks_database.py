56e65b9aeb8f7b3174d61b09c1c933976931d7c4
Scott <ryukaider@gmail.com>
2020-06-07 21:04:55 -0700

    Use stocks tables in stocks database (#41)

--- stocks/databases/stocks_database.py
+++ stocks/databases/stocks_database.py
@@ -1,11 +1,16 @@
 from databases.database import Database
-from databases.tables.api_progress_table import ApiProgressTable
-from databases.tables.tickers_table import TickersTable
+from databases import tables
 
 
 class StocksDatabase(Database):
     def __init__(self, name):
         Database.__init__(self, name)
+
         self.name = name
-        self.tickers_table = TickersTable(self.cursor())
-        self.api_progress_table = ApiProgressTable(self.cursor())
+
+        self.tickers_table = tables.tickers_table.TickersTable(self.cursor())
+        self.api_progress_table = tables.api_progress_table.ApiProgressTable(self.cursor())
+        self.company_profile_table = tables.company_profile_table.CompanyProfileTable(self.cursor())
+        self.daily_history_table = tables.daily_history_table.DailyHistoryTable(self.cursor())
+        self.yearly_history_table = tables.yearly_history_table.YearlyHistoryTable(self.cursor())
+        self.dividends_table = tables.dividends_table.DividendsTable(self.cursor())
