08fde8d42426cf7f59c21b3c9b7852bb6a0aedf6
Scott <ryukaider@gmail.com>
2020-06-24 22:25:00 -0700

    Remove unused stock class (#72)

--- stocks/stock.py
+++
@@ -1,6 +0,0 @@
-class Stock:
-
-    def __init__(self):
-        self.ticker = None
-        self.exchange = None
-        self.name = None
--- tests/test_stock.py
+++
@@ -1,6 +0,0 @@
-from stock import Stock
-
-
-def test_stock_exists():
-    new_stock = Stock()
-    assert new_stock is not None
