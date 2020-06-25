32565e78c6ce04acbe815f070fdbc3be3556fa78
Scott <ryukaider@gmail.com>
2020-06-24 21:12:13 -0700

    Reorganize docs (#67)

--- stocks/tests/utilities/test_json_utilities.py
+++ stocks/tests/utilities/test_json_utilities.py
@@ -5,7 +5,7 @@ _root_path = os.path.join(os.path.dirname(__file__), '..', '..')
 
 
 def test_read_json():
-    filepath = os.path.join(_root_path, '..', 'samples', 'alpha_vantage_monthly_history.json')
+    filepath = os.path.join(_root_path, '..', 'external_api_samples', 'alpha_vantage_monthly_history.json')
     json = json_utilities.read_json_file(filepath)
     assert json is not None
     assert len(json) > 0
